18e4108c34c0b504b52b2b20d4f2b8030ecb4797
Ishaaq Ahmed <a.ishaaq@icloud.com>
2024-10-31 09:44:42 +0000

    This version does have issues. The hex number 300 produces 3. I think it works for smaller numbers. I'll try fix it today.

--- ex04/ft_convert_base.c
+++ ex04/ft_convert_base.c
@@ -1,64 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-int ft_strlen(char *str)
-{
-    int i;
-
-    i = 0;
-    while(str[i] != '\0')
-        i++;
-    return (i);
-}
-int	indexInString(char target, char *str)
-{
-	int	i;
-	
-	while(str[i] != 0)
-	{	
-		if (str[i] == target)
-			return (i);
-		i ++;
-	}
-	return (-1);
-}
-
-int base_validation(char *str)
-{
-	// checks if base length is less than 2
-	// checks if base characters contain '+' or '-'
-	// checks if base characters contain duplicates
-
-	int	i;
-	int	j;
-	
-	i = 0;
-	if (ft_strlen(str) < 2)
-		return (1);
-	while(str[i] != '\0')
-	{
-		if (str[i] == '+' || str[i] == '-')
-			return (1);
-		j = i + 1;
-		while(str[j] != '\0')
-		{
-			if (str[i] == str[j])
-				return (1);
-			j ++;
-		}
-		i ++;
-	}
-	return (0);
-}
+int	ft_strlen(char *str);
+int	indexInString(char target, char *str);
+int	base_validation(char *str);
+char    *ft_putnbr_base(int nbr, char *base);
 
 char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 {
 	int	i;
 	int	sign;
 	int	base_from_len;
+	int	base_to_len;
 	int	index;
 	int	total;
-
+	
 	i = 0;
 	sign = 1;
 	if (base_validation(base_from) + base_validation(base_to) != 0)
@@ -70,22 +27,20 @@ char	*ft_convert_base(char *nbr, char *base_from, char *base_to)
 		i ++;
 	}
 	base_from_len = ft_strlen(base_from);
-	index = indexInString(nbr[i], base_from);
-	printf("%d\n", base_from_len);
-	//intf("%d\n", ft_strlen(
-	printf("%d\n", index);
-	while (index != -1)
+	base_to_len = ft_strlen(base_to);
+	total = 0;
+	
+	while (indexInString(nbr[i], base_from) != -1)
 	{
-		total = total * base_from_len + index;
-		i ++;
-		index = indexInString(nbr[i], base_from);
+		printf("%d\n", indexInString(nbr[i], base_from));
+		total = (total * base_from_len) + indexInString(nbr[i], base_from); 
+		i ++;	
 	}
-	printf("%d", total);
-	return ("hello");
+	return (ft_putnbr_base(total, base_to)); 
 }
 
 int	main(void)
 {
-	ft_convert_base("B2A", "0123456789ABCDEF", "0123456789");	
+	printf("%s\n", ft_convert_base("300", "0123456789ABCDEF", "0123456789"));	
 	return (0);
 }
--- ex04/ft_convert_base2.c
+++ ex04/ft_convert_base2.c
@@ -1,22 +1,92 @@
-#include <stdio.h>
 #include <stdlib.h>
 
-int	ft_strlen(char *str);
-
-char    *ft_convert_base(char *nbr, char *base_from, char * base_to)
+int ft_strlen(char *str)
 {
-	/*
-     if (ft_strlen(base_from) < 2 || ft_strlen(base_to) < 2 || base_validation(base) == 1)
-                printf("ERROR: Base if faulty");
-	*/
+    int i;
 
-	char	*string = "Hello world";
-	
-	printf("%d", ft_strlen(string));
-	return (string);
+    i = 0;
+    while(str[i] != '\0')
+        i++;
+    return (i);
 }
 
-int	main(void)
+int     indexInString(char target, char *str)
 {
-	return (0);
+        int     i;
+
+        while(str[i] != '0')
+        {
+                if (str[i] == target)
+                        return (i);
+                i ++;
+        }
+        return (-1);
 }
+
+int base_validation(char *str)
+{
+        // checks if base length is less than 2
+        // checks if base characters contain '+' or '-'
+        // checks if base characters contain duplicates
+
+        int     i;
+        int     j;
+
+        i = 0;
+        if (ft_strlen(str) < 2)
+                return (1);
+        while(str[i] != '\0')
+        {
+                if (str[i] == '+' || str[i] == '-')
+                        return (1);
+                j = i + 1;
+                while(str[j] != '\0')
+                {
+                        if (str[i] == str[j])
+                                return (1);
+                        j ++;
+                }
+                i ++;
+        }
+        return (0);
+}
+
+char    *append_char(char *string, char c)
+ {
+         char    *s;
+         int     i;
+         int     s_len;
+
+         s_len = ft_strlen(string);
+         s = malloc((s_len + 2) * sizeof(char));
+         s[0] = c;
+         i = 1;
+         while (i <= s_len)
+         {
+                 s[i] = string[i-1];
+                 i ++;
+         }
+         s[i] = 0;
+         free(string);
+         return (s);
+ }
+ 
+char	*ft_putnbr_base(int nbr, char *base)
+ {
+         int     base_len;
+         char    *string;
+
+	if (base_validation(base) == 1)
+        	return " ";
+	base_len = ft_strlen(base);
+        string = malloc(1 * sizeof(char));
+        while (nbr > 0)
+	{ 
+		string = append_char(string, base[nbr % base_len]); 
+        	nbr = nbr / base_len;
+        }
+         //write(1, &base[nbr % base_len], 1);
+         //write(1, string, ft_strlen(string));
+        return (string);
+}
+
